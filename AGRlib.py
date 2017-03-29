242e1e73b41a6f20a83573171b783b3ef3472d91
Jim Kadin <james.kadin@jax.org>
2017-03-29 15:57:35 -0400

    made stripNulls() recursive

--- AGRlib.py
+++ AGRlib.py
@@ -2,18 +2,23 @@
 
 import types
 import time
+import json
 # See: http://henry.precheur.org/projects/rfc3339 
 from rfc3339 import rfc3339
 
 #-----------------------------------
 # The AGR spec is to leave out attributes that would otherwise have a null
 #   value or an empty list value. This function removes these from a Json obj.
-#  (should this recurse?)
 #
 def stripNulls(obj):
     for k,v in obj.items():
 	if v is None or type(v) is types.ListType and len(v) == 0:
 	    del obj[k]
+	elif type(v) is types.ListType:
+	    for i in v:
+		if type(i) is types.DictType: stripNulls(i)
+	elif type(v) is types.DictType:
+	    stripNulls(v)
     return obj
 
 #-----------------------------------
@@ -48,4 +53,18 @@ def buildMetaObject(service):
     "dateProduced" : currentDate,
     "release" : release
     }
+
+if __name__ == "__main__":
+    obj =  \
+    {   "keep1" : 23,
+	"lose1" : None,
+	"lose2" : [],
+	"keep2" : [23, 64, 'hike', [],
+		    { 'keep3': 78, 'lose3': [], 'lose4':None }]
+    }
+    print "Before"
+    print json.dumps(obj, sort_keys=True, indent=2, separators=(',', ': ') )
+    print "After stripNulls"
+    print json.dumps(stripNulls(obj), sort_keys=True, indent=2, separators=(',', ': ') )
+
 
