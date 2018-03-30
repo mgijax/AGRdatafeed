5b21eff35a50df37683d34753222c8f8d0601268
Joel Richardson <joel.richardson@jax.org>
2018-03-30 14:22:02 -0400

    Remove some unneeded files.

--- bin/all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PYTHON=python
-${PYTHON} basicGeneInfo.py > bgi.json
-python agr_validate.py -s gene/geneMetaData.json -d ../AGRdatafeed/bgi.json
-
-${PYTHON} alleleInfo.py > alleles.json
-python agr_validate.py  -s allele/alleleMetaData.json -d ../AGRdatafeed/alleles.json
-
-${PYTHON} diseaseAnnotations.py > disease.json
