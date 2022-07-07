b01f92ed1518743fff918481ad9302d556b2cc2e
Joel Richardson <joel.richardson@jax.org>
2022-07-07 06:19:23 -0400

    (CRM-216) Add Driver Gene IDs to the allele Alliance feed

--- bin/construct.py
+++ bin/construct.py
@@ -2,12 +2,15 @@
 # contruct.py
 #
 # Script to generate construct objects for alleles in MGI.
-# Alleles that have expressed components or that have driver genes are considered to
-# have constructs.
-
+#
+# Alleles that have expressed components and/or driver genes are considered to
+# have constructs. One construct is created per allele, which  may contain multiple components.
+# (e.g., multiple expressed components, or an expressed component and a driver, or ...)
+#
 # The alliance ingest schema wants an identifier for each construct. MGI does not have
 # actual construct objects, much less identifiers for them. So this script creates ersatz
-# IDs for the constructs, based on the owning allele's id.
+# IDs for the constructs, based on the owning allele's id. If an allele's id is MGI:123456,
+# its contruct has the id MGI:123456_con
 #
 
 import sys
@@ -29,6 +32,11 @@ def loadSubmittedAlleles () :
         aids.add(a["primaryIdentifier"])
     return aids
     
+ak2nmdId = {} # allele_key -> non-mouse-driver ID
+def loadNonMouseDrivers () :
+    for r in sql(qNonMouseDrivers):
+        ak2nmdId[r['_allele_key']] = r['accid']
+
 def loadRelationship (key) :
     rels = []
     # read the properties and create index from rel key -> prop obj
@@ -44,11 +52,9 @@ def loadRelationship (key) :
     return rels
 
 def rel2constrComp (r) :
-    gid = r["gene"]
-    symbol = r["genesymbol"]
-
     if r["_category_key"] == EXPRESSES_cat_key:
         reln = "expresses"
+        symbol = r["genesymbol"]
         try:
             if r["relationship"] == "expresses_an_orthologous_gene":
                 symbol = r["properties"]["Non-mouse_Gene_Symbol"]
@@ -61,16 +67,21 @@ def rel2constrComp (r) :
                     gid = "ZFIN:" + r["properties"].get("Non-mouse_ZFIN_Gene_ID",None)
                 else:
                     gid = None
+            else:
+                gid = r["gene"]
         except:
             log("Error processing: " + str(r))
             return None
     elif r["_category_key"] == DRIVER_cat_key:
         reln = "is_regulated_by"
+        symbol = r["genesymbol"]
+        if r["_organism_key"] == "1":
+            gid = r["gene"]
+        else:
+            gid = ak2nmdId.get(r["_allele_key"], None)
     else:
         raise RuntimeError("Internal error: unknown _category_key: " + str(r))
 
-    if gid == "":
-        gid = None
     return {
         "componentSymbol" : symbol,
         "componentID" : gid,
@@ -111,8 +122,10 @@ qRelationships = '''
     SELECT 
         r._relationship_key,
         r._category_key,
+        al._allele_key,
         aa.accid as allele,
         al.symbol as alleleSymbol,
+        mm._organism_key,
         am.accid as gene,
         mm.symbol as genesymbol,
         rr.term as relationship,
@@ -157,6 +170,22 @@ qProperties = '''
     WHERE r._category_key = %s
     ORDER BY r._relationship_key, p.sequenceNum
     '''
+
+# query for non-mouse drivers used in recombinase alleles
+qNonMouseDrivers = '''
+    SELECT a.accid, r._object_key_1 as _allele_key
+    FROM
+        MGI_Relationship r,
+        MRK_Marker m,
+        ACC_Accession a
+    WHERE
+        r._category_key = %s
+    AND r._object_key_2 = m._marker_key
+    AND m._organism_key != 1
+    AND a._object_key = m._marker_key
+    AND a._mgitype_key = 2
+    AND a._logicaldb_key in (64,47,172) /* HGNC, RGD, ZFIN */
+    ''' % DRIVER_cat_key 
 
 # query for alleles being submitted to the Alliance. This is a MOUSEMINE query!
 qAlleles = '''<query
